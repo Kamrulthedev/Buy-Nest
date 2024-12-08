2c8e65768579db42a10fbb3c9a033c943f37c0df
Kamrul Hassan <kamrulthedev@gmail.com>
2024-12-09 01:55:38 +0600

    update-hero-section-commit

--- src/components/Bennar/Marquee.tsx
+++ src/components/Bennar/Marquee.tsx
@@ -2,16 +2,64 @@ import ReactFastMarquee from "react-fast-marquee";
 
 const MarqueeComponent = () => {
   return (
-    <div>
+    <div className="pt-10 text-center font-serif">
+      <div className="pt-12">
+        <h1 className="text-2xl text-gray-800">
+          Join <span className="text-rose-400">10,000+</span> successful brands choosing Minimog to build & grow their Shopify stores!
+        </h1>
+      </div>
       <ReactFastMarquee>
         <div>
-          <ul className="flex gap-10 text-5xl font-bold uppercase mt-20 items-center">
-            <img src="https://i.ibb.co.com/QYN9VjH/img-press-03.webp" alt="" />
-            <img src="https://i.ibb.co.com/zFBpn2z/img-press-01.webp" alt="" />
+          <ul className="flex gap-16 items-center">
+            <li>
+              <img
+                src="https://i.ibb.co.com/Mh2FRdP/image.png"
+                alt=""
+                className="w-44 h-44 object-contain"
+              />
+            </li>
+            <li>
+              <img
+                src="https://i.ibb.co.com/CV0nTWh/image.png"
+                alt=""
+                className="w-44 h-44 object-contain"
+              />
+            </li>
+            <li>
+              <img
+                src="https://i.ibb.co.com/HqQx06P/image.png"
+                alt=""
+                className="w-44 h-44 object-contain"
+              />
+            </li>
+            <li>
+              <img
+                src="https://i.ibb.co.com/Gxdp0RC/image.png"
+                alt=""
+                className="w-44 h-44 object-contain"
+              />
+            </li>
+            <li>
+              <img
+                src="https://i.ibb.co.com/XJTxf21/image.png"
+                alt=""
+                className="w-44 h-44 object-contain"
+              />
+            </li>
+
+            <li>
+              <img
+                src="https://i.ibb.co.com/K7234qv/image.png"
+                alt=""
+                className="w-44 h-44 object-contain"
+              />
+            </li>
+
           </ul>
         </div>
       </ReactFastMarquee>
     </div>
+
   );
 };
 
