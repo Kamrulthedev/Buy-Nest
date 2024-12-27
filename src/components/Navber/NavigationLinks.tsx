3f3a58879d6576cd7c69ae0b4cbe05c3aad5636c
Kamrul Hassan <kamrulthedev@gmail.com>
2024-12-28 02:00:31 +0600

    Update-navigation-links

--- src/components/Navber/NavigationLinks.tsx
+++ src/components/Navber/NavigationLinks.tsx
@@ -7,18 +7,19 @@ const NavigationLinks = () => {
             {/* Shop Dropdown */}
             <div className="relative group lg:mt-2">
                 <Link className="lg:text-lg hover:bg-slate-100 p-2 hover:rounded-lg" to="/products">Shop »</Link>
-                <div className="absolute top-full left-0 hidden group-hover:block bg-white shadow-lg rounded-lg w-72 z-10 animate__animated animate__fadeInDown">
+                <div className="absolute top-full left-0 hidden group-hover:block bg-white shadow-lg rounded-lg w-96 z-10 animate__animated animate__fadeInDown">
                     <div className="flex p-7">
                         <div>
-                            <Link className="block px-4 py-2 hover:bg-slate-100 rounded-lg" to="/products/category1">Category 1</Link>
-                            <Link className="block px-4 py-2 hover:bg-slate-100 rounded-lg" to="/products/category2">Category 2</Link>
-                            <Link className="block px-4 py-2 hover:bg-slate-100 rounded-lg" to="/products/category3">Category 3</Link>
-                            <Link className="block px-4 py-2 hover:bg-slate-100 rounded-lg" to="/products/category4">Category 4</Link>
+                            <Link className="block px-4 py-2 hover:bg-slate-100 rounded-lg" to="/products/category1">Man</Link>
+                            <Link className="block px-4 py-2 hover:bg-slate-100 rounded-lg" to="/products/category2">Woman</Link>
+                            <Link className="block px-4 py-2 hover:bg-slate-100 rounded-lg" to="/products/category3">Toys & Games</Link>
+                            <Link className="block px-4 py-2 hover:bg-slate-100 rounded-lg" to="/products/category4">Books</Link>
                         </div>
                         <div>
-                            <Link className="block px-4 py-2 hover:bg-slate-100 rounded-lg" to="/products/category1">Category 1</Link>
-                            <Link className="block px-4 py-2 hover:bg-slate-100 rounded-lg" to="/products/category2">Category 2</Link>
-                            <Link className="block px-4 py-2 hover:bg-slate-100 rounded-lg" to="/products/category3">Category 3</Link>
+                            <Link className="block px-4 py-2 hover:bg-slate-100 rounded-lg" to="/products/category1">Baby Products
+                            </Link>
+                            <Link className="block px-4 py-2 hover:bg-slate-100 rounded-lg" to="/products/category2">Jewelry</Link>
+                            <Link className="block px-4 py-2 hover:bg-slate-100 rounded-lg" to="/products/category3">Groceries</Link>
                             <Link className="block px-4 py-2 hover:bg-slate-100 rounded-lg" to="/products">All Shop List</Link>
                         </div>
                     </div>
