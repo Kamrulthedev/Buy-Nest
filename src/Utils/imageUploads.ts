93d2a05b6c233d4f4f26701281348a42917f531d
Kamrul Hassan <kamrulthedev@gmail.com>
2025-03-04 20:06:18 +0600

    export-image-upload-fucntion

--- src/Utils/imageUploads.ts
+++ src/Utils/imageUploads.ts
@@ -1,11 +1,24 @@
 export const imageUpload = async (imageFile: File) => {
     const formData = new FormData();
-    formData.append('image', imageFile)
-    const apiKey = ""
+    formData.append('image', imageFile);
+
+    const apiKey = "26ca840c6bddf0fe4d1eed5918a380a0"; 
+    const apiUrl = `https://api.imgbb.com/1/upload?key=${apiKey}`;
+
     try {
-        const res = await fetch(`?key=${apiKey}`, {
+        const res = await fetch(apiUrl, {
             method: "POST",
-            body: 
-    })
+            body: formData
+        });
+
+        if (!res.ok) throw new Error("Network response was not ok");
+
+        const data = await res.json();
+        if (!data.success) throw new Error("Image Upload failed");
+
+        return data.data.url; 
+    } catch (error) {
+        console.error("Error Uploading Image:", error);
+        return null;
     }
-}
+};
