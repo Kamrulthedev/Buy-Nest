3d407181448fbbc741735bb29463389c016c8708
Kamrul Hassan <kamrulthedev@gmail.com>
2025-03-04 19:58:49 +0600

    runing-function

--- src/Utils/imageUploads.ts
+++ src/Utils/imageUploads.ts
@@ -1,6 +1,11 @@
 export const imageUpload = async (imageFile: File) => {
-  const apiKey = ""
-  try{
-    
-  }
+    const formData = new FormData();
+    formData.append('image', imageFile)
+    const apiKey = ""
+    try {
+        const res = await fetch(`?key=${apiKey}`, {
+            method: "POST",
+            body: 
+    })
+    }
 }
