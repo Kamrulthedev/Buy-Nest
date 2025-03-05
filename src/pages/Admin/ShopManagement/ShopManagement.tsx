cec174b19be021ac0ca85b500c340c75f7ab60fc
Kamrul Hassan <kamrulthedev@gmail.com>
2025-03-05 19:04:37 +0600

    update-table

--- src/pages/Admin/ShopManagement/ShopManagement.tsx
+++ src/pages/Admin/ShopManagement/ShopManagement.tsx
@@ -65,7 +65,7 @@ const ShopManagement = () => {
                     <tbody>
                         {filteredShops.map((shop: any) => (
                             <tr className="text-xs animate__animated animate__fadeInDown" key={shop.id}>
-                                
+
                                 <td className="px-4 py-2 text-center">
                                     <img
                                         src={shop.logoUrl}
@@ -79,16 +79,17 @@ const ShopManagement = () => {
                                 <td className="px-4 py-2">{shop?.products?.length}</td>
                                 <td className="px-4 py-2">{shop?.orders?.length}</td>
                                 <td className="px-4 py-2">{shop?.followers?.length}</td>
-                                <td className="px-4 py-2 lg:space-y-0 space-y-3 flex gap-2">
+
+                                <td className=" px-4 py-2 space-y-2 md:space-y-0 md:space-x-2 flex flex-col md:flex-row justify-center">
                                     <Link
                                         to={`/admin/shop-details/${shop.id}`}
-                                        className="bg-blue-600 text-white px-4 py-2 rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-300"
+                                        className="bg-blue-500 text-white px-4 py-1 rounded-lg hover:bg-blue-600 w-full md:w-auto"
                                     >
                                         Details
                                     </Link>
                                     <button
                                         onClick={() => handleDeleteClick(shop.id)}
-                                        className="bg-red-600 text-white px-4 py-2 rounded-lg shadow-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 transition duration-300"
+                                        className="bg-red-500 text-white px-4 py-1 rounded-lg hover:bg-red-600 w-full md:w-auto"
                                     >
                                         Delete
                                     </button>
